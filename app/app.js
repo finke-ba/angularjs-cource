6b5e60bbbe14dac84a20520ce06e0065230e3068
finke.ba <finke.ba@gmail.com>
2017-01-23 11:37:51 +0300

    Add price and fix $http.get for idea httpserver.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var app = angular.module ('mobileStore', ['store-directives']);
 app.controller ('StoreController', ['$http', function ($http) {
   var store      = this;
   store.products = [];
-  $http.get ('/store-products.json').success (function (data) {
+  $http.get ('store-products.json').success (function (data) {
     store.products = data;
   });
 }]);
